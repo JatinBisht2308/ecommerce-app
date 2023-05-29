54438bc9d92e46b0278b0370151dd62debb1b0bc
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-05-30 02:41:48 +0530

    :fire: created the basic frontend of the adding new product page without any logic

--- ecommerce-admin/pages/products/new.js
+++ ecommerce-admin/pages/products/new.js
@@ -1,9 +1,19 @@
 import Layout from "@/components/layout";
-
+import { useState } from "react";
 export default function NewProduct() {
+    const [title,setTitle] = useState('');
+    const [description,setDescription] = useState('');
+    const [price,setPrice] = useState('');
   return (
     <Layout>
-      <h1>Add new product</h1>
+        <h1> New Product</h1>
+        <label>Product name</label>
+      <input type="text" placeholder="product name" value={title}/>
+      <label>Description</label>
+      <textarea placeholder='description' value={description}></textarea>
+      <label>Price (in INR)</label>
+      <input type="number" placeholder="price" value={price}/>
+      <button className="btn-primary">Save</button>
     </Layout>
   );
 }
