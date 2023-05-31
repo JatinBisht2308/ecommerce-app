649a4e207dfff909736c1be61ee3c551f2794cf7
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-06-01 03:13:27 +0530

    :coffee: stuck in between creating logic of uploading image because of AWS

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -6,6 +6,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -41,6 +42,20 @@ export default function ProductForm({
     // when we create a new product then we need to come back to the add new product page.
     setGoToProducts(true);
   }
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await fetch('/api/upload',{
+      method: 'POST',
+      body: data,
+      });
+      console.log(res);
+    }
+  }
   if (goToProducts) {
     router.push("/products");
   }
@@ -55,6 +70,32 @@ export default function ProductForm({
           setTitle(ev.target.value);
         }}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-28 h-28 border items-center text-center flex flex-col justify-center text-gray-500 font-semibold hover:text-white hover:bg-blue-900 rounded-lg bg-gray-300 delay-200 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && (
+          <div className="text-red-600 font-semibold">
+            **No images of this product.
+          </div>
+        )}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
